e50b116a128f16a970173cb74e95d1b5100cdc22
Vasilii Tokarev <notsoyunng@gmail.com>
2024-08-04 17:41:13 +0200

    add class with auxiliary functions

---
+++ auxiliary_functions.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+class Utils:
+
+    @staticmethod
+    def missing_values_percentage(df: pd.DataFrame) -> None:
+        missing_values_df_level = (df.notna().all(axis=1).sum() / df.shape[0]).round(2)
+        print(f'Complete rows in percentage: {missing_values_df_level}')
+
+        missing_values = ((df.isna().sum() / df.shape[0]) * 100).sort_values(ascending=False).round(2)
+        print(f'Missing values in percentage:\n{missing_values[missing_values > 0]}')
