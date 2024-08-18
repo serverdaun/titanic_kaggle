82f6074ceb2a4d2fb5acf13e7dcdb983743a3635
Vasilii Tokarev <notsoyunng@gmail.com>
2024-08-18 22:42:26 +0200

    enhance function to return boundaries of IQR

--- auxiliary_functions.py
+++ auxiliary_functions.py
@@ -1,4 +1,6 @@
+from typing import Tuple, Any, List
 import pandas as pd
+from pandas import Series, DataFrame
 
 
 class Utils:
@@ -6,7 +8,7 @@ class Utils:
     @staticmethod
     def missing_values_percentage(df: pd.DataFrame) -> None:
         """Functions finds complete rows in percentage and rows with missing values along with the percentage and then
-        prints out results"""
+        prints out results."""
         missing_values_df_level = (df.notna().all(axis=1).sum() / df.shape[0])
         print(f'Complete rows in percentage: {missing_values_df_level * 100:.2f}%')
 
@@ -15,11 +17,16 @@ class Utils:
 
 
     @staticmethod
-    def find_outliers_iqr(df:pd.DataFrame, column: pd.DataFrame.columns) -> pd.DataFrame:
-        """Functions finds outliers based on IQR and returns them as dataframe"""
+    def find_outliers_iqr(df:pd.DataFrame, column: pd.DataFrame.columns):
+        """Functions finds outliers based on IQR. It returns outliers as dataframe and IQR boundaries as a list."""
         q1 = df[column].quantile(0.25)
         q3 = df[column].quantile(0.75)
         iqr = q3 - q1
 
-        outliers = df[(df[column] < (q1 - 1.5 * iqr)) | (df[column] > (q3 + 1.5 * iqr))]
-        return outliers
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+        boundaries = [lower_bound, upper_bound]
+
+        outliers = df[(df[column] < lower_bound) | (df[column] > upper_bound)]
+
+        return outliers, boundaries
