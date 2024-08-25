7e1a350d2330a79c30562fd7a8017cc7d62a9ebd
Vasilii Tokarev <notsoyunng@gmail.com>
2024-08-25 21:26:20 +0200

    add main.ipynb for test predictions generation

---
+++ main.py
@@ -0,0 +1,19 @@
+import dill
+import pandas as pd
+
+with open('model/titanic_classifier_model.pkl', 'rb') as f:
+    model = dill.load(f)
+
+test_df = pd.read_csv('data/test.csv')
+
+predictions = model['model'].predict(test_df)
+
+prediction_df = pd.DataFrame({
+    'PassengerId': test_df['PassengerId'],
+    'Survived': predictions
+})
+
+output_file_path = 'titanic_predictions.csv'
+prediction_df.to_csv(output_file_path, index=False)
+
+print(f"Predictions saved to {output_file_path}")
